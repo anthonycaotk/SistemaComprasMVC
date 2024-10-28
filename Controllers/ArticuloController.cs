1f0f1d938f02e86d859aa500cfb6aab6d7c17756
JuniorMVP <cesarjunior1212@gmail.com>
2024-10-28 13:42:46 -0400

    Prueba commit, linea 9 art.controller

--- Controllers/ArticuloController.cs
+++ Controllers/ArticuloController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SistemaComprasMVC.Data;
-using SistemaComprasMVC.Models;
+using SistemaComprasMVC.Models;  // prueba git
 
 namespace SistemaComprasMVC.Controllers
 {
