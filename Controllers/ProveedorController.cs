4393fc8a25995e0d3feb0e39f4662eea01219df1
JuniorMVP <cesarjunior1212@gmail.com>
2024-10-29 00:24:42 -0400

    se agrego validacion de cedula y RNC

--- Controllers/ProveedorController.cs
+++ Controllers/ProveedorController.cs
@@ -22,9 +22,9 @@ namespace SistemaComprasMVC.Controllers
         // GET: Proveedor
         public async Task<IActionResult> Index()
         {
-              return _context.Proveedores != null ? 
-                          View(await _context.Proveedores.ToListAsync()) :
-                          Problem("Entity set 'SistemaComprasContext.Proveedores'  is null.");
+            return _context.Proveedores != null ?
+                        View(await _context.Proveedores.ToListAsync()) :
+                        Problem("Entity set 'SistemaComprasContext.Proveedores' is null.");
         }
 
         // GET: Proveedor/Details/5
@@ -52,12 +52,23 @@ namespace SistemaComprasMVC.Controllers
         }
 
         // POST: Proveedor/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,CedulaORNC,NombreComercial,Estado")] Proveedor proveedor)
         {
+            if (!esCedulaValida(proveedor.CedulaORNC) && !esUnRNCValido(proveedor.CedulaORNC))
+            {
+                ModelState.AddModelError("CedulaORNC", "La cédula o RNC es inválido.");
+            }
+            else if (!esCedulaValida(proveedor.CedulaORNC))
+            {
+                ModelState.AddModelError("CedulaORNC", "La cédula es inválida.");
+            }
+            else if (!esUnRNCValido(proveedor.CedulaORNC))
+            {
+                ModelState.AddModelError("CedulaORNC", "El RNC es inválido.");
+            }
+
             if (ModelState.IsValid)
             {
                 _context.Add(proveedor);
@@ -84,8 +95,6 @@ namespace SistemaComprasMVC.Controllers
         }
 
         // POST: Proveedor/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,CedulaORNC,NombreComercial,Estado")] Proveedor proveedor)
@@ -143,21 +152,68 @@ namespace SistemaComprasMVC.Controllers
         {
             if (_context.Proveedores == null)
             {
-                return Problem("Entity set 'SistemaComprasContext.Proveedores'  is null.");
+                return Problem("Entity set 'SistemaComprasContext.Proveedores' is null.");
             }
             var proveedor = await _context.Proveedores.FindAsync(id);
             if (proveedor != null)
             {
                 _context.Proveedores.Remove(proveedor);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool ProveedorExists(int id)
         {
-          return (_context.Proveedores?.Any(e => e.Id == id)).GetValueOrDefault();
+            return (_context.Proveedores?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+        // Métodos de validación
+        public static bool esCedulaValida(string pCedula)
+        {
+            int vnTotal = 0;
+            string vcCedula = pCedula.Replace("-", "");
+            int pLongCed = vcCedula.Trim().Length;
+            int[] digitoMult = new int[11] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1 };
+
+            if (pLongCed != 11) // Cambiar a "no es igual a 11"
+                return false;
+
+            for (int vDig = 1; vDig <= pLongCed; vDig++)
+            {
+                int vCalculo = Int32.Parse(vcCedula.Substring(vDig - 1, 1)) * digitoMult[vDig - 1];
+                if (vCalculo < 10)
+                    vnTotal += vCalculo;
+                else
+                    vnTotal += Int32.Parse(vCalculo.ToString().Substring(0, 1)) + Int32.Parse(vCalculo.ToString().Substring(1, 1));
+            }
+            return vnTotal % 10 == 0;
+        }
+
+        private bool esUnRNCValido(string pRNC)
+        {
+            int vnTotal = 0;
+            int[] digitoMult = new int[8] { 7, 9, 8, 6, 5, 4, 3, 2 };
+            string vcRNC = pRNC.Replace("-", "").Replace(" ", "");
+
+            if (vcRNC.Length != 9)
+                return false;
+
+            string vDigito = vcRNC.Substring(8, 1);
+
+            if (!"145".Contains(vcRNC.Substring(0, 1)))
+                return false;
+
+            for (int vDig = 1; vDig <= 8; vDig++)
+            {
+                int vCalculo = Int32.Parse(vcRNC.Substring(vDig - 1, 1)) * digitoMult[vDig - 1];
+                vnTotal += vCalculo;
+            }
+
+            return (vnTotal % 11 == 0 && vDigito == "1") ||
+                   (vnTotal % 11 == 1 && vDigito == "1") ||
+                   ((11 - (vnTotal % 11)).Equals(vDigito));
         }
     }
 }
--- Models/Proveedor.cs
+++ Models/Proveedor.cs
@@ -1,10 +1,57 @@
-﻿namespace SistemaComprasMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaComprasMVC.Models
 {
     public class Proveedor
     {
         public int Id { get; set; } // Identificador
+
+        [Required(ErrorMessage = "La cédula o RNC es obligatorio.")]
+        [RegularExpression(@"^[0-9]{1,3}-?[0-9]{1,9}$", ErrorMessage = "Formato de cédula o RNC no válido.")]
         public string CedulaORNC { get; set; } // Cédula o RNC
+
+        [Required(ErrorMessage = "El nombre comercial es obligatorio.")]
         public string NombreComercial { get; set; } // Nombre del proveedor
+
         public bool Estado { get; set; } // Estado (activo/inactivo)
+
+        public static bool ValidaCedula(string pCedula)
+        {
+            int vnTotal = 0;
+            string vcCedula = pCedula.Replace("-", "");
+            int pLongCed = vcCedula.Trim().Length;
+            int[] digitoMult = new int[11] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1 };
+            if (pLongCed != 11)
+                return false;
+            for (int vDig = 1; vDig <= pLongCed; vDig++)
+            {
+                int vCalculo = Int32.Parse(vcCedula.Substring(vDig - 1, 1)) * digitoMult[vDig - 1];
+                if (vCalculo < 10)
+                    vnTotal += vCalculo;
+                else
+                    vnTotal += Int32.Parse(vCalculo.ToString().Substring(0, 1)) + Int32.Parse(vCalculo.ToString().Substring(1, 1));
+            }
+            return vnTotal % 10 == 0;
+        }
+
+        public static bool EsUnRNCValido(string pRNC)
+        {
+            int vnTotal = 0;
+            int[] digitoMult = new int[8] { 7, 9, 8, 6, 5, 4, 3, 2 };
+            string vcRNC = pRNC.Replace("-", "").Replace(" ", "");
+            string vDigito = vcRNC.Substring(8, 1);
+            if (vcRNC.Length != 9)
+                return false;
+            if (!"145".Contains(vcRNC.Substring(0, 1)))
+                return false;
+            for (int vDig = 1; vDig <= 8; vDig++)
+            {
+                int vCalculo = Int32.Parse(vcRNC.Substring(vDig - 1, 1)) * digitoMult[vDig - 1];
+                vnTotal += vCalculo;
+            }
+            return (vnTotal % 11 == 0 && vDigito == "1") ||
+                   (vnTotal % 11 == 1 && vDigito == "1") ||
+                   (11 - (vnTotal % 11)).Equals(vDigito);
+        }
     }
 }
